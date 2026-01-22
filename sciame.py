999ad2e1a07fab23816d5fd7d88bf6d0d5c30f5e
AlessioTorricelli <alessio.torricelli18@gmail.com>
2026-01-22 18:27:40 +0100

    Update sciame.py

--- sciame.py
+++ sciame.py
@@ -2,27 +2,25 @@
 Modulo sciame.py
 
 Contiene classi per particelle (elettroni e positroni) e fotoni, e una funzione di simulazione di uno sciame.
-
 """
-
 
 import numpy as np
 		
 class Particella:
 	
 	"""
-	Rappresenta una particella carica dello sciame
+	Rappresenta una particella carica dello sciame.
 	
 	Attributi:
 	
-	E (float): energia della particella
-	tipo (str): tipo della particella (elettrone o positrone)
+	E (float): Energia della particella [MeV]
+	tipo (str): Tipo della particella (elettrone o positrone)
 	
 	Metodi:
 	
-	evoluzione: simula l'evoluzione della particella in un passo
-	esclusione: esclude la particella dallo sciame
-	step: esegue un passo di evoluzione della particella
+	evoluzione: Simula l'evoluzione della particella in un passo
+	esclusione: Esclude la particella dallo sciame
+	step: Esegue un passo di evoluzione della particella
 	"""
 	
 	def __init__(self, E0, tipo):
@@ -38,32 +36,29 @@ class Particella:
 	def evoluzione(self, sciame, s, dE_X0, ec, E_ion):
 		
 		"""
-		Simula la perdita di energia per ionizzazione e l'eventuale emissione di un fotone per Bremsstrahlung 
+		Simula la perdita di energia per ionizzazione e l'eventuale emissione di un fotone per Bremsstrahlung.
 		
 		Parametri: 
 		
-		sciame (list): lista della particelle o fotoni allo step successivo
-		s (float): passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
-		dE_X0 (float): energia persa per ionizzazione in una lunghezza di radiazione
-		ec (float): energia critica della particella
-		E_ion (float): energia depositata per ionizzazione nel passo corrente
+		sciame (list): Lista delle particelle o fotoni presenti allo step successivo
+		s (float): Passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
+		dE_X0 (float): Energia persa per ionizzazione in una lunghezza di radiazione [MeV/cm]
+		ec (float): Energia critica della particella [MeV]
+		E_ion (float): Energia depositata per ionizzazione nel passo corrente [MeV]
 		
 		Ritorna:
 		
-		E_ion (float): energia depositata aggiornata (dopo l'evoluzione)
+		E_ion (float): energia depositata aggiornata (dopo l'evoluzione) [MeV]
 		"""
 		
 		if self.E > dE_X0 * s:
-				
 			self.E = self.E - dE_X0 * s
 			E_ion += dE_X0 * s
 		
 		if self.E > ec :
-				
 			p = np.random.random()
 			
 			if p > np.exp( -s ):
-				
 				sciame.append( Fotone(self.E/2) )
 				self.E = self.E/2
 			
@@ -74,52 +69,48 @@ class Particella:
 	def esclusione(self, E_ion):
 		
 		"""
-		Esclude una particella dallo sciame aggiornando l'energia persa per ionizzazione
+		Esclude una particella dallo sciame aggiornando l'energia persa per ionizzazione.
 		
 		Parametri:
 		
-		E_ion (float): energia depositata per ionizzazione nel passo corrente
+		E_ion (float): energia depositata per ionizzazione nel passo corrente [MeV]
 		
 		Ritorna:
 		
-		E_ion (float): energia depositata aggiornata
+		E_ion (float): energia depositata aggiornata (dopo l'esclusione) [MeV]
 		"""
 		
 		En = np.random.uniform(0, self.E)
 		E_ion += En
-		self.E = 0
 		return E_ion
 		
 	def step(self, s, sciame, E_ion, ec_elettrone, ec_positrone, dE_X0):
 		
 		"""
-		Simula un passo per una particella
+		Simula un passo per una particella.
 		
 		Parametri:
 		
-		s (float): passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
-		sciame (list): lista della particelle o fotoni presenti allo step successivo
-		E_ion (float): energia depositata per ionizzazione prima che la particella esegua lo step
-		ec_elettrone (float): energia critica per gli elettroni nel materiale considerato
-		ec_positrone (float): energia critica per i positroni nel materiale considerato
-		dE_X0 (float): energia persa per ionizzazione in una lunghezza di radiazione
+		s (float): Passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
+		sciame (list): Lista della particelle o fotoni presenti allo step successivo
+		E_ion (float): Energia depositata per ionizzazione prima che la particella esegua lo step [MeV]
+		ec_elettrone (float): Energia critica per gli elettroni nel materiale considerato [MeV]
+		ec_positrone (float): Energia critica per i positroni nel materiale considerato [MeV]
+		dE_X0 (float): Energia persa per ionizzazione in una lunghezza di radiazione [MeV/cm]
 		
 		Ritorna:
 		
-		E_ion: energia totale depositata per ionizzazione dopo lo step
+		E_ion: energia totale depositata per ionizzazione nello step [MeV]
 		"""
 		
 		if self.E < dE_X0 * s:
-		
 			E_ion = self.esclusione(E_ion)
 	
 		else:
 			if self.tipo == 'elettrone':
-			
 				E_ion = self.evoluzione(sciame, s, dE_X0, ec_elettrone, E_ion)
 		
 			else:
-			
 				E_ion = self.evoluzione(sciame, s, dE_X0, ec_positrone, E_ion)
 			
 		return E_ion
@@ -127,18 +118,18 @@ class Particella:
 class Fotone:
 	
 	"""
-	Rappresenta un fotone dello sciame
+	Rappresenta un fotone dello sciame.
 	
 	Attributi:
 	
-	E (float): energia del fotone
-	tipo (str): tipo della particella (fotone)
+	E (float): Energia del fotone [MeV]
+	tipo (str): Tipo della particella (fotone)
 	
 	Metodi:
 	
-	evoluzione: simula l'evoluzione della particella in un passo
-	esclusione: esclude la particella dallo sciame
-	step: esegue un passo di evoluzione della particella
+	evoluzione: Simula l'evoluzione della particella in un passo
+	esclusione: Esclude la particella dallo sciame
+	step: Esegue un passo di evoluzione della particella
 	"""
 	
 	def __init__(self, E0):
@@ -149,12 +140,12 @@ class Fotone:
 	def evoluzione(self, sciame, s):
 		
 		"""
-		Simula l'eventuale interazione di un fotone e l'emissione di un positrone e di un elettrone
+		Simula l'eventuale interazione di un fotone e l'emissione di un positrone e di un elettrone.
 		
 		Parametri:
 		
-		sciame (list): lista della particelle o fotoni presenti allo step successivo
-		s (float): passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
+		sciame (list): Lista delle particelle o fotoni presenti allo step successivo
+		s (float): Passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
 		
 		Ritorna:
 		
@@ -162,56 +153,55 @@ class Fotone:
 		"""
 		
 		p = np.random.random()
+		
 		if p > np.exp( -(7 * s )/ 9 ):
 			sciame.append( Particella(self.E/2, 'elettrone') )
 			sciame.append( Particella(self.E/2, 'positrone') )
+			
 		else:
 			sciame.append(self)
 	
 	def esclusione(self, E_ion):
 		
 		"""
-		Esclude una particella dallo sciame aggiornando l'energia persa per ionizzanione
+		Esclude il fotone dallo sciame aggiornando l'energia persa per ionizzanione.
 		
 		Parametri:
 		
-		E_ion (float): energia depositata per ionizzazione nel passo corrente
+		E_ion (float): Energia depositata per ionizzazione nel passo corrente [MeV]
 		
 		Ritorna:
 		
-		E_ion (float): energia depositata aggiornata
+		E_ion (float): Energia depositata aggiornata (dopo l'esclusione) [MeV]
 		"""
 		
 		En = np.random.uniform(0, self.E)
 		E_ion += En
-		self.E = 0
 		return E_ion
 		
 	def step(self, s, sciame, E_ion, ec_elettrone = None, ec_positrone = None, dE_X0 = None):
 	
 		"""
-		Simula un passo per un fotone
+		Simula un passo per un fotone.
 		
 		Parametri:
 		
-		s (float): passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
-		sciame (list): lista della particelle o fotoni presenti allo step successivo
-		E_ion (float): energia depositata per ionizzazione prima che il fotone esegua lo step
-		ec_elettrone (float, opzionale): non utilizzato nella funzione, presente per compatibilità con la classe particella
-		ec_positrone (float, opzionale): non utilizzato nella funzione, presente per compatibilità con la classe particella
-		dE_X0 (float, opzionale): non utilizzato nella funzione, presente per compatibilità con la classe particella
+		s (float): Passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
+		sciame (list): Lista della particelle o fotoni presenti allo step successivo
+		E_ion (float): Energia depositata per ionizzazione prima che il fotone esegua lo step [MeV]
+		ec_elettrone (float, opzionale): Non utilizzato nella funzione, presente per compatibilità con la classe particella [MeV]
+		ec_positrone (float, opzionale): Non utilizzato nella funzione, presente per compatibilità con la classe particella [MeV]
+		dE_X0 (float, opzionale): Non utilizzato nella funzione, presente per compatibilità con la classe particella [MeV/cm]
 		
 		Ritorna:
 		
-		E_ion(float): energia totale depositata per ionizzazione dopo lo step
+		E_ion(float): Energia totale depositata per ionizzazione dopo lo step [MeV]
 		"""
 		
 		if self.E > 2 * 0.511:
-		
 			self.evoluzione(sciame, s)
 		
 		else:
-		
 			E_ion = self.esclusione(E_ion)
 	
 		return E_ion
@@ -220,22 +210,22 @@ class Fotone:
 def simulazione(E0, ec_elettrone, ec_positrone, dE_X0, s, tipo):
 	
 	"""
-	Simula uno sciame elettromagnetico
+	Simula uno sciame elettromagnetico.
 	
 	Parametri:
 	
-	E0 (float): energia della particella iniziale
-	ec_elettrone (float): energia critica per gli elettroni nel materiale in esame
-	ec_positrone (float): energia critica per i positroni nel materiale in esame
-	dE_X0 (float): perdita per ionizzazione in una lunghezza di radiazione
-	s (float): passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
-	tipo (str): tipo della particella iniziale (elettrone, positrone, fotone) 
+	E0 (float): Energia della particella iniziale [MeV]
+	ec_elettrone (float): Energia critica per gli elettroni nel materiale in esame [MeV]
+	ec_positrone (float): Energia critica per i positroni nel materiale in esame [MeV]
+	dE_X0 (float): Perdita per ionizzazione in una lunghezza di radiazione [MeV/cm]
+	s (float): Passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1]) 
+	tipo (str): Tipo della particella iniziale (elettrone, positrone, fotone) 
 	
 	Ritorna:
 	
-	E_step(list): energia depositata per ionizzazione in ogni step
-	n_part(list): numero di particelle dello sciame in ogni step
-	E_tot(float): energia totale depositata per ionizzazione
+	E_step(list): Energia depositata per ionizzazione in ogni step [MeV]
+	n_part(list): Numero di particelle dello sciame in ogni step 
+	E_tot(float): Energia totale depositata per ionizzazione [MeV]
 	"""
 
 	sciame_i = []
@@ -248,14 +238,12 @@ def simulazione(E0, ec_elettrone, ec_positrone, dE_X0, s, tipo):
 		raise ValueError('Inserire valori di energia positivi')
 		
 	if s <= 0 or s > 1:
-		raise ValueError("Il passo s deve essere compreso nell'intervallo (0,1]")
+		raise ValueError("Il passo 's' deve essere compreso nell'intervallo (0,1]")
 	
 	if tipo == 'fotone':
-		
 		sciame_i.append(Fotone(E0))
 	
 	elif tipo == 'elettrone' or tipo == 'positrone':
-		
 		sciame_i.append(Particella(E0, tipo))
 	
 	else:
@@ -263,6 +251,7 @@ def simulazione(E0, ec_elettrone, ec_positrone, dE_X0, s, tipo):
 	
 	
 	while len(sciame_i) != 0:
+		
 		n_steps += 1
 		E_ion = 0
 		
@@ -278,4 +267,5 @@ def simulazione(E0, ec_elettrone, ec_positrone, dE_X0, s, tipo):
 		sciame_f = []			
 	
 	E_tot = np.sum(E_step)
+	
 	return E_step, n_part, E_tot
