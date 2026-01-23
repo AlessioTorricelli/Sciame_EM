ca40973e15c6d03b427d971c9b81dda91554318f
AlessioTorricelli <alessio.torricelli18@gmail.com>
2026-01-23 15:45:19 +0100

    Update analisi_sciame.py

--- analisi_sciame.py
+++ analisi_sciame.py
@@ -1,8 +1,7 @@
 """
 Modulo analisi_sciame.py
 
-Contiene le funzioni necessarie per analizzare l'andamento medio del profilo longitudinale dello sciame e dei suoi parametri 
-
+Contiene le funzioni necessarie per analizzare l'andamento medio del profilo longitudinale dello sciame e dei suoi parametri.
 """
 
 import numpy as np
@@ -11,46 +10,44 @@ import sciame
 def profilo_medio(E0, ec_elettrone, ec_positrone, dE_X0, s, tipo, n):
 	
 	"""
-	Simula uno sciame più volte e ne calcola i valori medi
+	Simula uno sciame più volte e ne calcola i valori medi.
 	
 	Parametri:
-		E0 (float): energia della particella iniziale [MeV]
-		ec_elettrone (float): energia critica per gli elettroni nel materiale in esame [MeV]
-		ec_positrone (float): energia critica per i positroni nel materiale in esame [MeV]
-		dE_X0 (float): perdita per ionizzazione in una lunghezza di radiazione [MeV/cm]
-		s (float): passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
-		tipo (str): tipo della particella iniziale (elettrone, positrone, fotone)
-		n (int): numero di simulazioni da eseguire
+		E0 (float): Energia della particella iniziale [MeV]
+		ec_elettrone (float): Energia critica per gli elettroni nel materiale in esame [MeV]
+		ec_positrone (float): Energia critica per i positroni nel materiale in esame [MeV]
+		dE_X0 (float): Perdita per ionizzazione in una lunghezza di radiazione [MeV/cm]
+		s (float): Passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
+		tipo (str): Tipo della particella iniziale (elettrone, positrone, fotone)
+		n (int): Numero di simulazioni da eseguire
 	
 	Ritorna:
 	risultati (dict): contiene
-		- 'E_med' (list): energia media depositata per ogni passo [MeV]
-        - 'E_err' (list): errore standard dell'energia media depositata [MeV]
-        - 'n_med' (list): numero medio di particelle per ogni passo
-        - 'n_err' (list): errore standard del numero medio di particelle
-        - 'E_cum_med' (list): energia cumulata media [MeV]
-        - 'E_cum_err' (list): errore standard dell'energia cumulata media [MeV]
-        - 'distanza' (list): coordinata longitudinale dello sciame in unità di X0 
+		- 'E_med' (list): Energia media depositata per ogni passo [MeV]
+        - 'E_err' (list): Errore standard dell'energia media depositata [MeV]
+        - 'n_med' (list): Numero medio di particelle per ogni passo
+        - 'n_err' (list): Errore standard del numero medio di particelle
+        - 'E_cum_med' (list): Energia cumulata media [MeV]
+        - 'E_cum_err' (list): Errore standard dell'energia cumulata media [MeV]
+        - 'distanza' (list): Coordinata longitudinale dello sciame (in unità di X0)
 	"""
 	if n <= 0:
-		raise ValueError(f'Il numero n di simulazioni da ripetere per ogni valore di energia deve essere > 0')
+		raise ValueError(f'Il numero n di simulazioni da ripetere per ogni valore di energia deve essere positivo')
 	
 	Energia = []
-	particelle = []
-
-
+	n_particelle = []
 
 	for i in range(n):
 		
 		E_step, n_part, E_tot = sciame.simulazione(E0, ec_elettrone, ec_positrone, dE_X0, s, tipo)
 		
 		Energia.append(E_step)
-		particelle.append(n_part)
+		n_particelle.append(n_part)
 		
-	massimo = max(len(p) for p in particelle)
+	massimo = max(len(p) for p in n_particelle)
 	mat_en = np.zeros((len(particelle), massimo))
 	mat_part = np.zeros((len(particelle), massimo))
-	for i in range(len(particelle)):
+	for i in range(n):
 		for j in range(massimo):
 			if len(Energia[i]) > j:
 				mat_en[i][j] = Energia[i][j]
@@ -85,41 +82,41 @@ def sciame_stat(E0_min, E0_max, materiali, s, tipo, nE, n):
 	Esegue più simulazioni dello sciame per diversi valori di energia spaziati logaritmicamente nell'intervallo dato.
 	
 	Parametri:
-		E0_min (float): valore minimo dell'intervallo di energie in cui vengono eseguite le simulazioni [MeV]
-		E0_max (float): valore massimo dell'intervallo di energie in cui vengono eseguite le simulazioni [MeV]
-		materiali (dict): contiene
-			'materiale' (list): nella lista sono presenti i seguenti valori nell'ordine
-								ec_elettrone (float): energia critica per gli elettroni nel materiale in esame [MeV]
-								ec_positrone (float): energia critica per i positroni nel materiale in esame [MeV]
-								dE_X0 (float): perdita per ionizzazione in una lunghezza di radiazione [MeV/cm]
-								X0 (float): lunghezza di radiazione [cm]
-								colore (str): nome del colore da utilizzare per rappresentare nei grafici il materiale 
-		s (float): passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
-		tipo (str): tipo della particella iniziale (elettrone, positrone, fotone)
-		nE (int): numero di valori di energia da considerare nell'intervallo scelto
-		n (int): numero di simulazioni da eseguire per ogni valore di energia
+		E0_min (float): Valore minimo dell'intervallo di energie in cui vengono eseguite le simulazioni [MeV]
+		E0_max (float): Valore massimo dell'intervallo di energie in cui vengono eseguite le simulazioni [MeV]
+		materiali (dict): Contiente le informazioni dei parametri in esame.
+			Ogni chiave è il nome del materiale (str) e il valore è una lista contenente:
+			-ec_elettrone (float): Energia critica per gli elettroni nel materiale in esame [MeV]
+			-ec_positrone (float): Energia critica per i positroni nel materiale in esame [MeV]
+			-dE_X0 (float): Perdita per ionizzazione in una lunghezza di radiazione [MeV/cm]
+			-X0 (float): Lunghezza di radiazione [cm]
+			-colore (str): Colore da utilizzare per rappresentare nei grafici il materiale 
+		s (float): Passo di avanzamento della simulazione in frazioni di X0 (s in (0, 1])
+		tipo (str): Tipo della particella iniziale (elettrone, positrone, fotone)
+		nE (int): Numero di valori di energia da considerare nell'intervallo scelto
+		n (int): Numero di simulazioni da eseguire per ogni valore di energia
 	
 	Ritorna:
-		Energie (np.array): array contenente le nE energie utilizzate
-		risultati (dict): contiene i risultati  per ogni materiale.
+		Energie (np.array): Contiene le nE energie utilizzate.
+		risultati (dict): Contiene i risultati  per ogni materiale.
 				Ogni chiave è il nome del materiale (str) e il valore è un dict contenente:
 				-'En' (list): energia totale media depositata per ionizzazione per ogni valore di energia [MeV]
 				-'En_err' (list): errore standard dell'energia totale media depositata [MeV]
 				-'n_max' (list): numero massimo medio di particelle presenti nello sciame per ogi valore di energia
 				-'n_max_err' (list): errore standard del numero massimo medio di particelle
 				-'dist_max' (list): distanza massima raggiunta in media dallo sciame per ogni valore di energia [cm]
-				-'dist_max_err' (list): errore standard della distanza massima raggiunta
+				-'dist_max_err' (list): errore standard della distanza massima raggiunta [cm]
 				-'massimo' (list): distanza media alla quale si ha il numero massimo di particelle per ogni valore di energua [cm]
-				-'massimo_err' (list): errore standard della distanza media alla quale si ha il numero massimo di particelle
+				-'massimo_err' (list): errore standard della distanza media alla quale si ha il numero massimo di particelle [cm]
 				-'color' (str): nome del colore da utilizzare per rappresentare nei grafici il materiale
 	"""
 	
-	if E0_max < 0:
-		raise ValueError('Inserire un valore di energia  positivo')
+	if E0_min < 0 or E0_max > 0:
+		raise ValueError('Inserire valori di energia  positivo')
 		
 	if nE < 0 or n < 0:
-		raise ValueError('nE e n devono essere entrambi positivi')
-	
+		raise ValueError('nE' e 'n' devono essere entrambi positivi')
+		
 	risultati = {}
 	
 	esponente_min = np.log10(E0_min)
@@ -128,21 +125,21 @@ def sciame_stat(E0_min, E0_max, materiali, s, tipo, nE, n):
 	
 	for materiale in materiali:
 		
-		En = []                 #energia media per ogni E0
+		En = []                 #Energia media depositata per ogni E0
 		En_err = []
 				
-		n_max = []				#numero massimo di particelle medio per ogni E0
+		n_max = []				#Numero massimo medio di particelle per ogni E0
 		n_max_err = []
 			
-		dist_max = []			#distanza massima media raggiunta dallo sciame per ogni E0
+		dist_max = []			#Distanza massima media raggiunta dallo sciame per ogni E0
 		dist_max_err = []
 				
-		massimo = []			#distanza media alla quale si trova il massimo dello sciame per ogni E0
+		massimo = []			#Distanza media alla quale si trova il massimo dello sciame per ogni E0
 		massimo_err = []
 
 		for i in range(nE):
 				
-			En_simulazione = []   		 # n energie simulate per una E0
+			En_simulazione = []   		 # n energie depositate per una E0
 			n_max_simulazione = []		 # n numeri massimi di particelle per una E0
 			n_passi = []		 		 # n numeri di passi eseguiti per una E0
 			indice_massimo = []	 		 # n indici dove si trova il valore del massimo per una E0 
@@ -159,16 +156,16 @@ def sciame_stat(E0_min, E0_max, materiali, s, tipo, nE, n):
 					
 				
 			En.append(np.mean(En_simulazione))
-			En_err.append(np.std(En_simulazione, ddof = 1)/np.sqrt(len(En_simulazione)))
+			En_err.append(np.std(En_simulazione, ddof = 1)/np.sqrt(n))
 				
 			n_max.append(np.mean(n_max_simulazione))
-			n_max_err.append(np.std(n_max_simulazione, ddof = 1)/np.sqrt(len(n_max_simulazione)))
+			n_max_err.append(np.std(n_max_simulazione, ddof = 1)/np.sqrt(n))
 				
 			dist_max.append(np.mean(n_passi) * s * materiali[materiale][3])
-			dist_max_err.append(np.std(n_passi, ddof = 1)/np.sqrt(len(n_passi)) * s * materiali[materiale][3])
+			dist_max_err.append(np.std(n_passi, ddof = 1)/np.sqrt(n) * s * materiali[materiale][3])
 			
 			massimo.append(np.mean(indice_massimo) * s * materiali[materiale][3])
-			massimo_err.append(np.std(indice_massimo, ddof = 1)/np.sqrt(len(indice_massimo)) * s * materiali[materiale][3])
+			massimo_err.append(np.std(indice_massimo, ddof = 1)/np.sqrt(n) * s * materiali[materiale][3])
 			
 			
 		risultati[materiale] = {'En': En,
